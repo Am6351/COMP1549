b129ae93fae1b427d404b7486b2082389934ba89
Am6351 <162934821+Am6351@users.noreply.github.com>
2024-03-11 01:49:43 +0000

    Update Message.java

--- src/message/Message.java
+++ src/message/Message.java
@@ -1,10 +1,43 @@
 package message;
 
-public class Message {
+import java.io.Serializable;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class Message implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private String senderId;
+    private String receiverId;
+    private String content;
+    
+    public Message(String senderId, String receiverId, String content) {
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.content = content;
+    }
+    
+    // Getters and setters
+    
+    public String getSenderId() {
+        return senderId;
+    }
+    
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+    
+    public String getReceiverId() {
+        return receiverId;
+    }
+    
+    public void setReceiverId(String receiverId) {
+        this.receiverId = receiverId;
+    }
+    
+    public String getContent() {
+        return content;
+    }
+    
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
