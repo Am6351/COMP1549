1dc325509acb652ed33e65018f55ee152a834351
Abdul <162934821+Am6351@users.noreply.github.com>
2024-03-13 16:08:02 +0000

    Add files via upload

---
+++ src/com/test/Client.java
@@ -0,0 +1,62 @@
+package com.test;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Client {
+    private String serverAddress;
+    private int serverPort;
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public Client(String serverAddress, int serverPort) {
+        this.serverAddress = serverAddress;
+        this.serverPort = serverPort;
+    }
+
+    public void connect() {
+        try {
+            socket = new Socket(serverAddress, serverPort);
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            System.out.println("Connected to server " + serverAddress + ":" + serverPort);
+
+            new Thread(new MessageReceiver()).start();
+
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                String message = scanner.nextLine();
+                sendMessage(message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage(String message) {
+        out.println(message);
+    }
+
+    private class MessageReceiver implements Runnable {
+        @Override
+        public void run() {
+            try {
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    System.out.println("Received message: " + inputLine);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String serverAddress = "localhost"; 
+        int serverPort = 12345; 
+        Client client = new Client(serverAddress, serverPort);
+        client.connect();
+    }
+}
